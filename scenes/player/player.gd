8970442c68e827b949a92e4f366c8730acbc8c01
Guterfps <ithayg@gmail.com.com>
2025-11-29 19:05:37 +0200

    added hud and connected it with main

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,7 +8,7 @@ var screen_size: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	#hide()
+	hide()
 
 func start(pos: Vector2) -> void:
 	position = pos
